5b3146d788a79bb6bc845208728d04b3c4a50077
GarThor <43391503+GarThor@users.noreply.github.com>
2024-04-27 12:03:06 -0700

    Initial commit: calculating volumes

--- 3D/Volumes.js
+++ 3D/Volumes.js
@@ -39,6 +39,12 @@ function ChangePrimative() {
     console.log("foobar!");
 
     switch (InPrimitive) {
+        case "cube":
+            console.log("case cube");
+            Hide(CylinderInputs);
+            Show(CubeInputs);
+
+            break;
         case "cylinder":
             console.log("case cylinder");
             console.log(CubeInputs.style)
@@ -46,11 +52,71 @@ function ChangePrimative() {
             Hide(CubeInputs);
 
             break;
-        case "cube":
-            console.log("case cube");
-            Hide(CylinderInputs);
-            Show(CubeInputs);
+    }
+}
 
+function ChangeMoldBoxUnits() {
+    // TBD: hook me up later!
+}
+
+function ChangeMoldVolumeUnits() {
+    // TBD!
+}
+
+function CalculateVolumes() {
+    console.log("Calculating splines!");
+
+    let Volume = 0;
+    let Units = "null";
+
+    // Do the volume calculation, and set units based on selected primative
+    var InPrimitive = document.getElementById("Primitive").value;
+    switch (InPrimitive) {
+        case "cube": {
+            console.log("cubic mold box");
+
+            let X = document.getElementById("X").value;
+            let Y = document.getElementById("Y").value;
+            let Z = document.getElementById("Z").value;
+
+            Volume = X * Y * Z;
+
+            let UnitObj = document.getElementById("CubeMoldBoxUnits");
+            Units = UnitObj.options[UnitObj.selectedIndex].getAttribute('name');;
+        } break;
+        case "cylinder": {
+            console.log("cylindric mold box");
+
+            let Radius = document.getElementById("Radius").value;
+            let Height = document.getElementById("Height").value;
+
+            Volume = Math.TAU * Radius * Height;
+
+            let UnitObj = document.getElementById("CylinderMoldBoxUnits");
+            Units = UnitObj.options[UnitObj.selectedIndex].getAttribute('name');;
+        } break;
+    }
+
+    console.log("Units = ", Units);
+    console.log("Volume = ", Volume);
+
+    // Make the units match!
+    switch (Units) {
+        case "inch":
+            Units = "cubic-inch";
+            break;
+        case "cm":
+            Units = "cubic-cm";
             break;
     }
+
+    console.log("Volume Units = ", Units);
+
+    document.getElementById("MoldVolumeUnits").selectedIndex = Units;
+    document.getElementById("SiliconeUnits").selectedIndex = Units;
+    document.getElementById("MoldVolume").value = Volume;
+
+    let ObjectVolume = document.getElementById("ObjVol").value;
+    let SiliconeVolume = Volume - ObjectVolume;
+    document.getElementById("SiliconeVolume").value = SiliconeVolume;
 }
